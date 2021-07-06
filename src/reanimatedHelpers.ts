f761cfe54bc395344cf4277929c40425de9a058b
Surya Chandra <surya.c@7t.co>
2021-07-06 12:47:13 -0500

    Migrates to  reanimated2

--- src/reanimatedHelpers.ts
+++ src/reanimatedHelpers.ts
@@ -1,22 +1,14 @@
-import Animated, { Easing } from 'react-native-reanimated';
+import Animated, { EasingNode } from 'react-native-reanimated';
 
-const {
-  Value,
-  set,
-  cond,
-  startClock,
-  clockRunning,
-  timing,
-  stopClock,
-  block,
-} = Animated;
+const { Value, set, cond, startClock, clockRunning, timing, stopClock, block } =
+  Animated;
 
 export function runTiming(
   clock: Animated.Clock,
   value: Animated.Value<number>,
   dest: Animated.Value<number>,
   duration: number = 250,
-  easing: Animated.EasingFunction = Easing.out(Easing.ease)
+  easing: Animated.EasingNodeFunction = EasingNode.out(EasingNode.ease)
 ) {
   const state = {
     finished: new Value(0),
